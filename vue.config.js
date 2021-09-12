d416f46c1bc0006a789bcbd0fba02771b104b99d
huangqiongzhen <2387555042@qq.com>
2021-09-12 22:38:53 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,44 @@
-debugger
-console.log(process.env)
-const path=require('path');
+const port = process.env.port || 8088
+const path = require('path')
 
-function resolve(dir){
-    return path.join(__dirname,dir)
+function resolve(dir) {
+  return path.join(__dirname, dir)
 }
-
-module.exports={
-    publicPath:'/',
-    outputDir:'dist',
-    assetsDir:'static',
-
-    
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  indexPath: 'index.html',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: 'https://www.fastmock.site/mock',
+        ws: true,
+        pathRewrite: {
+          [process.env.VUE_APP_BASE_API]: ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  // 设置别名
+  configureWebpack: {
+    name: 'vue2-vant-master',
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+        'components': resolve('src/components'),
+        'views': resolve('src/views'),
+        'utils': resolve('src/utils'),
+        'styles': resolve('src/styles')
+      }
+    }
+  }
 }
